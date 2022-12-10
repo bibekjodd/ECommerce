8c5bd2eb1d3146a1c23e2bc6903b7d56ea181852
Bibek Bhattarai <bibekjodd@outlook.com>
2022-12-10 07:54:09 +0545

    initial-setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='w-full flex flex-col items-center text-lg min-h-screen'>
+      <h1 className='w-full text-xl text-white bg-purple-400 text-center'>Hello world</h1>
+
+    </div>
+  )
+}
