b86e6e2853ad264542c8871cf323c4211c7a9ec6
Bibek Bhattarai <bibekjodd@outlook.com>
2022-12-13 20:52:35 +0545

    scroll to top on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useContext, useEffect } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import About from './pages/About'
@@ -11,6 +11,17 @@ import Products from './pages/Products'
 import SingleProduct from './pages/SingleProduct'
 
 function App() {
+
+    const location = useLocation();
+    useEffect(() => {
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        })
+    }, [location])
+
+
     return (
         <div className='min-h-screen flex flex-col '>
             <Header />
